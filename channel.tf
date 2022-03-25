2370df17471767930d400d523096c4f8be8a5736
guranytou <guranytou@gmail.com>
2022-03-25 18:49:56 +0900

    ファイル分割

---
+++ channel.tf
@@ -0,0 +1,25 @@
+resource "discord_category_channel" "textchannel" {
+  name      = "Text Channels"
+  server_id = discord_server.my_server.id
+  position  = 0
+}
+
+resource "discord_text_channel" "general" {
+  name      = "general"
+  server_id = discord_server.my_server.id
+  category  = discord_category_channel.textchannel.id
+  position  = 0
+}
+
+resource "discord_category_channel" "voicechannel" {
+  name      = "Voice Channels"
+  server_id = discord_server.my_server.id
+  position  = 0
+}
+
+resource "discord_voice_channel" "general" {
+  name      = "General"
+  server_id = discord_server.my_server.id
+  category  = discord_category_channel.voicechannel.id
+  position  = 0
+}
--- main.tf
+++ main.tf
@@ -15,36 +15,21 @@ variable "token" {
   type = string
 }
 
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "p4u2_discord_server"
+
+    workspaces {
+      name = "discord-sandbox"
+    }
+  }
+}
+
 resource "discord_server" "my_server" {
   name              = "gurany_touのサーバー"
   region            = "japan"
   system_channel_id = "955825994344853537"
-}
-
-resource "discord_category_channel" "textchannel" {
-  name      = "Text Channels"
-  server_id = discord_server.my_server.id
-  position  = 0
-}
-
-resource "discord_text_channel" "general" {
-  name      = "general"
-  server_id = discord_server.my_server.id
-  category  = discord_category_channel.textchannel.id
-  position  = 0
-}
-
-resource "discord_category_channel" "voicechannel" {
-  name      = "Voice Channels"
-  server_id = discord_server.my_server.id
-  position  = 0
-}
-
-resource "discord_voice_channel" "general" {
-  name      = "General"
-  server_id = discord_server.my_server.id
-  category  = discord_category_channel.voicechannel.id
-  position  = 0
 }
 
 
