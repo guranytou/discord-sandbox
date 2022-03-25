b8edd31ddd6e2c6a0c3ff2170d149775c9a61de4
guranytou <guranytou@gmail.com>
2022-03-25 23:28:24 +0900

    test channnel position change

--- channel.tf
+++ channel.tf
@@ -15,7 +15,7 @@ resource "discord_text_channel" "test" {
   name      = "test_channnel"
   server_id = discord_server.my_server.id
   category  = discord_category_channel.textchannel.id
-  position  = 1
+  position  = 0
 }
 
 resource "discord_category_channel" "voicechannel" {
