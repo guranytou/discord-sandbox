ee3baa6cf41463959b503139f796d999290fceb4
guranytou <guranytou@gmail.com>
2022-03-27 19:24:53 +0900

    add bad_role

--- role.tf
+++ role.tf
@@ -19,12 +19,16 @@ data "discord_color" "blue" {
   hex = "#4287f5"
 }
 
-# resource "discord_role" "bad_role" {
-#   server_id   = discord_server.my_server.id
-#   name        = "bad_role"
-#   permissions = data.discord_permission.member.allow_bits
-#   color       = data.discord_color.blue.dec
-#   hoist       = true
-#   mentionable = true
-#   position    = 1
-# }
+resource "discord_role" "bad_role" {
+  server_id   = discord_server.my_server.id
+  name        = "bad_role"
+  permissions = 0
+  color       = data.discord_color.bad.dec
+  hoist       = false
+  mentionable = true
+  position    = 2
+}
+
+data "discord_color" "bad" {
+    hex = "#e65323"
+}
