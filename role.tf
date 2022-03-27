b7925c32595e06563fba842ca0ca96ede8e11c1f
guranytou <guranytou@gmail.com>
2022-03-27 16:59:38 +0900

    add test role1

---
+++ role.tf
@@ -0,0 +1,20 @@
+resource "discord_role" "test_role1" {
+  server_id   = discord_server.my_server.id
+  name        = "test_role1"
+  permissions = data.discord_permission.member.allow_bits
+  color       = data.discord_color.blue.dec
+  hoist       = true
+  mentionable = true
+  position    = 2
+}
+
+data "discord_permission" "member" {
+  view_channel     = "allow"
+  send_messages    = "allow"
+  use_vad          = "deny"
+  priority_speaker = "deny"
+}
+
+data "discord_color" "blue" {
+  hex = "#4287f5"
+}
